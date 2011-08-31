e291eac7cf67a4b4a4a67d0eef8af4935f8694f6
Rocky Bernstein <rocky@cpan.org>
2011-08-30 22:56:08 -0400

    Add uniq_abbrev and fill out tests for that.

--- lib/Devel/Trepan/Util.pm
+++ lib/Devel/Trepan/Util.pm
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Rocky Bernstein <rocky@cpan.org>
 package Devel::Trepan::Util;
 use vars qw(@EXPORT @EXPORT_OK @ISA);
-@EXPORT    = qw( hash_merge safe_repr);
+@EXPORT    = qw( hash_merge safe_repr uniq_abbrev);
 @ISA = qw(Exporter);
 
 # Hash merge like Ruby has.
@@ -19,13 +21,27 @@ sub safe_repr($$;$)
     my $strlen = length($str);
     if ($max > 0 && $strlen > $max && -1 == index($str, "\n")) {
 	sprintf("%s%s%s", substr($str, 0, $max/2), 
-		$elipsis,  substr($str, ($strlen-$max)/2));
+		$elipsis,  substr($str, $strlen+1-($max)/2));
     } else {
 	$str;
     }
 }
 
-if (__FILE__ eq $0 ) {
+# name is String and list is an Array of String.
+# If name is a unique leading prefix of one of the entries of list,
+# then return that. Otherwise return name.
+sub uniq_abbrev($$)
+{ 
+    my ($list, $name) = @_;
+    my @candidates = ();
+    for my $try_name (@$list) {
+	push @candidates, $try_name if 0 == index($try_name, $name);
+    }
+    scalar @candidates == 1 ? $candidates[0] : $name;
+}
+
+
+unless (caller) {
     my $default_config = {a => 1, b => 'c'};
     require Data::Dumper;
     import Data::Dumper;
@@ -44,8 +60,15 @@ if (__FILE__ eq $0 ) {
     my $string = 'The time has come to talk of many things.';
     print safe_repr($string, 50), "\n";
     print safe_repr($string, 17), "\n";
-    print safe_repr($string, 17, '');
+    print safe_repr($string, 17, ''), "\n";
 
+    my @list = qw(disassemble disable distance up);
+    uniq_abbrev(\@list, 'disas');
+    print join(' ', @list), "\n";
+    for my $name (qw(dis disas u upper foo)) {
+	printf("uniq_abbrev of %s is %s\n", $name, 
+	       uniq_abbrev(\@list, $name));
+    }
 }
 
 1;
--- t/test-util.t
+++ t/test-util.t
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use lib '../lib';
 
-use Test::More tests => 4;
+use Test::More 'no_plan';
 note( "Testing Devel::Trepan::Util" );
 
 BEGIN {
@@ -11,6 +11,8 @@ use_ok( 'Devel::Trepan::Util' );
 }
 
 import Devel::Trepan::Util;
+
+note 'Test hash_merge';
 my $default_config = {a => 1, b => 'c', term_adjust=>0};
 my $config = {};
 hash_merge $config, $default_config;
@@ -25,3 +27,25 @@ is($config->{term_adjust}, 1,
     "Should pick up supplied value over default");
 is($config->{bogus}, 'yep',
     "Should pick up supplied value");
+
+
+note 'Test safe_repr';
+my $string = 'The time has come to talk of many things.';
+is(safe_repr($string, 50), $string);
+is(safe_repr($string, 17), 'The time...  things.');
+
+note 'Test uniq_abbrev';
+
+my @list = qw(disassemble disable distance up);
+for my $pair 
+    (
+     ['dis', 'dis'],
+     ['disas', 'disassemble'],
+     ['u', 'up'],
+     ['upper', 'upper'],
+     ['foo', 'foo']) {
+	my ($name, $expect) = @$pair;
+	is(uniq_abbrev(\@list, $name), $expect);
+}
+
+

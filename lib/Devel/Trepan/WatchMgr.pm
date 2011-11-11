beca4f8e9a82ecf943b8acdeee779110d0b9d1a6
Rocky Bernstein <rocky@cpan.org>
2011-11-10 19:22:25 -0500

    Add watchpoint manager. Closer to having watchpoints working...

---
+++ lib/Devel/Trepan/WatchMgr.pm
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Rocky Bernstein <rockyb@rubyforge.net>
+use strict; use warnings; no warnings 'redefine';
+use English qw( -no_match_vars );
+use relative_lib '../..';
+
+use Class::Struct;
+use strict;
+
+struct WatchPoint => {
+    expr        => '$', # what Perl expression to evaluate
+    old_value   => '$', # Previous value
+    current_val => '$', # Current value. Set only when != old value
+    id          => '$', # watchpoint number
+};
+
+package WatchPoint;
+sub inspect($)
+{
+    my $self = shift;
+    sprintf("watchpoint %d, expr %s, old_value: %s, current_value %s",
+	    $self->id, $self->expr, $self->old_value // 'undef',
+	    $self->current_val // 'undef',
+	);
+};
+
+package Devel::Trepan::WatchMgr;
+
+sub new($$) 
+{
+    my ($class,$dbgr) = @_;
+    my $self = {};
+    $self->{dbgr} = $dbgr;
+    bless $self, $class;
+    $self->clear();
+    $self;
+}
+
+sub clear($) 
+{
+    my $self = shift;
+    $self->{list} = [];
+    $self->{next_id} = 1;
+}    
+
+sub inspect($) 
+{
+    my $self = shift;
+    my $str = '';
+    for my $watchpoint ($self->list) {
+	next unless defined $watchpoint;
+	$str .= $watchpoint->inspect . "\n";
+    }
+    $str;
+}    
+
+sub list($)
+{
+    my $self = shift;
+    return @{$self->{list}};
+    
+}
+
+# Remove all breakpoints that we have recorded
+sub DESTROY() {
+    my $self = shift;
+    for my $id ($self->list) {
+        $self->delete_by_object($id) if defined($id);
+    }
+    $self->{clear};
+}
+
+sub find($$)
+{
+    my ($self, $index) = @_;
+    for my $object ($self->list) {
+	next unless $object;
+	return $object if $object->id eq $index;
+    }
+    return undef;
+}
+
+sub delete($$)
+{
+    my ($self, $index) = @_;
+    my $object = $self->find($index);
+    if (defined ($object)) {
+        $self->delete_by_object($object);
+        return $object;
+    } else {
+        return undef;
+    }
+}
+
+sub delete_by_object($$)
+{
+    my ($self, $delete_object) = @_;
+    for my $candidate ($self->list) {
+	next unless defined $candidate;
+	if ($candidate eq $delete_object) {
+	    $candidate = undef;
+	    return $delete_object;
+	}
+    }
+    return undef;
+}
+
+sub add($$)
+{
+    my ($self, $watchpoint) = @_;
+    push @{$self->{list}}, $watchpoint;
+    return $watchpoint;
+}
+
+sub compact($)
+{
+    my $self = shift;
+    my @new_list = ();
+    for my $watchpoint ($self->list) {
+	next unless defined $watchpoint;
+	push @new_list, $watchpoint;
+    }
+    $self->{list} = \@new_list;
+    return $self->{list};
+}
+
+sub is_empty($)
+{
+    my $self = shift;
+    $self->compact();
+    return scalar(0 == $self->list);
+}
+
+sub max($)
+{
+    my $self = shift;
+    my $max = 0;
+    for my $watchpoint ($self->list) {
+	$max = $watchpoint->id if $watchpoint->id > $max;
+    }
+    return $max;
+}
+
+sub size($)
+{
+    my $self = shift;
+    $self->compact();
+    return scalar $self->list;
+}
+
+sub reset($)
+{
+    my $self = shift;
+    for my $id ($self->list) {
+     	$self->{dbgr}->delete_object($id);
+     }
+    $self->{list} = [];
+}
+
+
+unless (caller) {
+
+    sub wp_status($$)
+    { 
+	my ($watchpoints, $i) = @_;
+	printf "list size: %s\n", $watchpoints->size();
+	printf "max: %d\n", $watchpoints->max() // -1;
+	print $watchpoints->inspect();
+	print "--- ${i} ---\n";
+    }
+
+# require Devel::Trepan::Core;
+# my $dbgr = Devel::Trepan::Core->new;
+my $watchpoints = Devel::Trepan::WatchMgr->new('bogus');
+wp_status($watchpoints, 0);
+my $watchpoint1 = WatchPoint->new(
+    expr=>'1+2', id=>1, current_val => 3 );
+
+$watchpoints->add($watchpoint1);
+wp_status($watchpoints, 1);
+
+my $watchpoint2 = WatchPoint->new(
+    expr=>'3*4', id=>2, current_val => 12
+    );
+$watchpoints->add($watchpoint2);
+wp_status($watchpoints, 2);
+
+$watchpoints->delete_by_object($watchpoint1);
+wp_status($watchpoints, 3);
+
+my $watchpoint3 = WatchPoint->new(
+    expr=>'3*4+5', id=>3, current_val => 17
+    );
+$watchpoints->add($watchpoint3);
+wp_status($watchpoints, 4);
+
+
+
+  # p watchpoints.delete(2)
+  # p watchpoints[2]
+  # wp_status(watchpoints, 3)
+
+  # # Two of the same breakpoints but delete 1 and see that the
+  # # other still stays
+  # offset = frame.pc_offset
+  # b2 = Trepan::Breakpoint.new(iseq, offset)
+  # watchpoints << b2
+  # wp_status(watchpoints, 4)
+  # b3 = Trepan::Breakpoint.new(iseq, offset)
+  # watchpoints << b3
+  # wp_status(watchpoints, 5)
+  # watchpoints.delete_by_object(b2)
+  # wp_status(watchpoints, 6)
+  # watchpoints.delete_by_object(b3)
+  # wp_status(watchpoints, 7)
+}
+
+1;

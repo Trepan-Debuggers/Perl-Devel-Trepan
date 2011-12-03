baf1b7ed2db27c55c040b8516204b3f034111cf7
Rocky Bernstein <rocky@cpan.org>
2011-12-03 10:50:18 -0500

    Add "show interactive - gdb has something similar

---
+++ lib/Devel/Trepan/CmdProcessor/Command/Show_Subcmd/Interactive.pm
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Rocky Bernstein <rocky@cpan.org>
+use warnings; no warnings 'redefine'; no warnings 'once';
+use rlib '../../../../..';
+
+package Devel::Trepan::CmdProcessor::Command::Show::Interactive;
+
+use Devel::Trepan::CmdProcessor::Command::Subcmd::Core;
+
+@ISA = qw(Devel::Trepan::CmdProcessor::Command::ShowBoolSubcmd);
+# Values inherited from parent
+use vars @Devel::Trepan::CmdProcessor::Command::Subcmd::SUBCMD_VARS;
+
+our $HELP = "Show whether debugger input is a terminal";
+our $MIN_ABBREV = length('inter');
+
+sub run($$)
+{
+    my ($self, $args) = @_;
+    my $proc = $self->{proc};
+    my $bool =  $proc->{interfaces}->[-1]->is_interactive();
+    my $msg = sprintf("Debugger's interactive mode is %s.",  
+		      $self->show_onoff($bool));
+    $proc->msg($msg);
+}
+
+unless (caller) {
+  # Demo it.
+  # require_relative '../../mock'
+
+  # # FIXME: DRY the below code
+  # my ($dbgr, $cmd) = MockDebugger::setup('show');
+  # $subcommand = __PACKAGE__->new(cmd);
+  # $testcmdMgr = Trepan::Subcmd->new(subcommand);
+
+  # $subcommand->run_show_bool();
+  # $subcommand->summary_help($NAME);
+}
+
+1;

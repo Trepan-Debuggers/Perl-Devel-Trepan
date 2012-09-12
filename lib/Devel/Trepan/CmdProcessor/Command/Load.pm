502345a3adfcfb475c7e1e9db73fd62e89899f43
Rocky Bernstein <rocky@cpan.org>
2012-09-12 17:52:31 -0400

    Add command to allow loading a debugger command or command directory.

---
+++ lib/Devel/Trepan/CmdProcessor/Command/Load.pm
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011-2012 Rocky Bernstein <rocky@cpan.org>
+use warnings; no warnings 'redefine';
+use rlib '../../../..';
+
+package Devel::Trepan::CmdProcessor::Command::Load;
+
+use if !@ISA, Devel::Trepan::CmdProcessor::Command::Subcmd::SubMgr;
+use if !@ISA, Devel::Trepan::CmdProcessor::Command;
+unless (@ISA) {
+    eval <<'EOE';
+use constant SHORT_HELP => 'Load or something Perlis'; 
+use constant CATEGORY => 'status';
+use constant MIN_ARGS   => 0;  # Need at least this many
+use constant MAX_ARGS   => undef; # Need at most this many - undef -> unlimited.
+use constant NEED_STACK => 0;
+EOE
+}
+
+use strict;
+use vars qw(@ISA);
+@ISA = qw(Devel::Trepan::CmdProcessor::Command::SubcmdMgr);
+use vars @CMD_VARS;
+
+our $NAME       = set_name();
+our $HELP = <<'HELP';
+=pod
+
+Generic command for loading or reloading.
+
+You can give unique prefix of the name of a subcommand to get
+information about just that subcommand.
+
+Type C<info> for a list of "load" subcommands and what they do.
+Type C<help load *> for a list of C<load> subcommands.
+=cut
+HELP
+
+unless (caller) {
+    require Devel::Trepan::CmdProcessor;
+    my $proc = Devel::Trepan::CmdProcessor->new(undef, 'bogus');
+    my $cmd = Devel::Trepan::CmdProcessor::Command::Show->new($proc, $NAME);
+    # require_relative '../mock'
+    # dbgr, cmd = MockDebugger::setup
+    $cmd->run([$NAME])
+}
+
+1;
---
+++ lib/Devel/Trepan/CmdProcessor/Command/Load_Subcmd/Command.pm
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011-2012 Rocky Bernstein <rocky@cpan.org>
+use warnings; no warnings 'redefine'; no warnings 'once';
+use rlib '../../../../..';
+
+package Devel::Trepan::CmdProcessor::Command::Load::Command;
+use Cwd 'abs_path';
+
+use Devel::Trepan::CmdProcessor::Command::Subcmd::Core;
+use Devel::Trepan::DB::LineCache;
+
+use strict;
+our (@ISA, @SUBCMD_VARS);
+# Values inherited from parent
+use vars @Devel::Trepan::CmdProcessor::Command::Subcmd::SUBCMD_VARS;
+
+our @DEFAULT_FILE_ARGS = qw(size mtime sha1);
+our $DEFAULT_FILE_ARGS = join(' ', @DEFAULT_FILE_ARGS);
+
+## FIXME: do automatically.
+our $CMD = "info files";
+
+unless (@ISA) {
+    eval <<"EOE";
+    use constant MAX_ARGS => 8;  # Need at most this many - undef -> unlimited.
+EOE
+}
+
+@ISA = qw(Devel::Trepan::CmdProcessor::Command::Subcmd);
+
+our $HELP = <<'HELP';
+=pod
+
+load commmand {I<filename>|directory-name}
+
+Load a debugger command or directory of debugger commands.
+=cut
+HELP
+
+our $SHORT_HELP = 'Load debugger command(s)';
+our $MIN_ABBREV = length('fi');
+
+# sub complete($$)
+# {
+#     my ($self, $prefix) = @_;
+#     my @completions = ('.', DB::LineCache::file_list());
+#     Devel::Trepan::Complete::complete_token(\@completions, $prefix);
+# }
+
+sub run($$) 
+{
+    my ($self, $args) = @_;
+    my $proc = $self->{proc};
+    my @args = @$args; shift @args; shift @args;
+    foreach my $file_or_dir (@args) {
+	$proc->load_debugger_commands($file_or_dir);
+    }
+}
+
+unless (caller) {
+    require Devel::Trepan;
+    # Demo it.
+    # require_relative '../../mock'
+    # my($dbgr, $parent_cmd) = MockDebugger::setup('show');
+    # $cmd = __PACKAGE__->new(parent_cmd);
+    # $cmd->run(@$cmd->prefix);
+}
+
+# Suppress a "used-once" warning;
+$HELP || scalar @SUBCMD_VARS;
